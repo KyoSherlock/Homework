1da1387e60a05d50cde7e7a32ed4fb6d1a4fa5d0
Hui Jiang <jianghui-2000@163.com>
2017-11-30 22:40:20 +0800

    unit test, add Thread.sleep for notify delay.

--- app/src/androidTest/java/com/kyo/homework/MomentsActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/kyo/homework/MomentsActivityInstrumentedTest.java
@@ -2,6 +2,7 @@ package com.kyo.homework;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -18,7 +19,10 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -32,6 +36,9 @@ import static org.hamcrest.EasyMock2Matchers.equalTo;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MomentsActivityInstrumentedTest {
+
+    private static final long SWIP_INTERVAL = 2000;
+
     private IdlingResource idlingresource;
 
     @Rule
@@ -51,8 +58,102 @@ public class MomentsActivityInstrumentedTest {
     }
 
     @Test
-    public void testListInitLoad() {
-        onData(withItemContent("沙发！")).check(matches(isDisplayed()));
+    public void testPage1Load() {
+        // page 1
+        onView(withText("沙发！")).check(matches(isDisplayed()));
+        onView(withText("Joe Portman")).check(matches(isDisplayed()));
+        onView(withText("Super hero:Good.\nDoggy Over:Like it too")).check(matches(isDisplayed()));
+        onView(withText("Coolzzz")).check(matches(isDisplayed()));
+        onView(withText(PAGE_FIRST_ITEM)).check(doesNotExist());
+    }
+
+    @Test
+    public void testPage2Load() {
+        // page 2
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        onView(withText(PAGE_FIRST_ITEM)).check(matches(isDisplayed()));
+        onView(withText("Rebecca")).check(doesNotExist());
+    }
+
+    @Test
+    public void testPage3Load() {
+        // page 2
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText(PAGE_FIRST_ITEM)).check(matches(isDisplayed()));
+        onView(withText("Rebecca")).check(doesNotExist());
+
+
+        // page 3
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("Rebecca")).check(matches(isDisplayed()));
+        onView(withText("第10条！")).check(doesNotExist());
+    }
+
+    @Test
+    public void testPage4Load() {
+        // page 2
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText(PAGE_FIRST_ITEM)).check(matches(isDisplayed()));
+        onView(withText("Rebecca")).check(doesNotExist());
+
+        // page 3
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("Rebecca")).check(matches(isDisplayed()));
+        onView(withText("第10条！")).check(doesNotExist());
+
+        // page 4
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("第10条！")).check(matches(isDisplayed()));
+        onView(withText("Louis")).check(doesNotExist());
+    }
+
+
+    @Test
+    public void testPage5Load() {
+        // page 2
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText(PAGE_FIRST_ITEM)).check(matches(isDisplayed()));
+        onView(withText("Rebecca")).check(doesNotExist());
+
+        // page 3
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("Rebecca")).check(matches(isDisplayed()));
+        onView(withText("第10条！")).check(doesNotExist());
+
+        // page 4
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("第10条！")).check(matches(isDisplayed()));
+        onView(withText("Louis")).check(doesNotExist());
+
+        // page 5
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        waitForPageRefresh();
+        onView(withText("Louis")).check(matches(isDisplayed()));
+    }
+
+    private void waitForPageRefresh(){
+        try {
+            Thread.sleep(SWIP_INTERVAL);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testPullRefresh() {
+        // refresh
+        onView(withId(R.id.recycler_view)).perform(swipeUp());
+        onView(withId(R.id.recycler_view)).perform(swipeDown());
+        onView(withText("沙发！")).check(matches(isDisplayed()));
+        onView(withText(PAGE_FIRST_ITEM)).check(doesNotExist());
     }
 
     @After
@@ -64,4 +165,6 @@ public class MomentsActivityInstrumentedTest {
         checkNotNull(expectedText);
         return withItemContent(equalTo(expectedText));
     }
+
+    private static final String PAGE_FIRST_ITEM = "Unlike many proprietary big data processing platform different, Spark is built on a unified abstract RDD, making it possible to deal with different ways consistent with large data processing scenarios, including MapReduce, Streaming, SQL, Machine Learning and Graph so on. To understand the Spark, you have to understand the RDD. ";
 }
